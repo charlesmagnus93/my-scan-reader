b8b24f63d38159cfd08b1b764ba26daead722e95
charlemagne <cagbolossou@dactechnologies.net>
2021-07-16 10:34:30 +0000

    part 3 finish

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-import { heading, nav, active } from '../components/layout.module.css'
+import { heading, nav, active, container } from '../components/layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
   return (
@@ -10,8 +10,10 @@ const Layout = ({ pageTitle, children }) => {
         <Link to="/" className={active}>Home</Link>
         <Link to="/about">About</Link>
       </div>
-      <h1 className={heading}>{pageTitle}</h1>
-      {children}
+      <div className={container}>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </div>
     </main>
   )
 }
